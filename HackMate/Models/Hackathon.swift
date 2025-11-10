f155c6ba8e6c5a066727adbe57c7f89b782d664a
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-11-10 11:07:54 +0530

    fix: Hackathon Data model fixed for eligibility and rules

--- HackMate/Models/Hackathon.swift
+++ HackMate/Models/Hackathon.swift
@@ -1,33 +1,33 @@
 //
-//  Hackathon.swift
-//  HackMate
+//  Hackathon.swift
+//  HackMate
 //
-//  Created by Manik Lakhanpal on 09/11/25.
+//  Created by Manik Lakhanpal on 09/11/25.
 //
 
 import Foundation
 
 struct Hackathon: Codable, Identifiable {
-    var id: String = UUID().uuidString
-    var bannerImage: String? // URL or image name
-    var name: String
-    var tagline: String
-    var description: String
-    var dates: HackathonDates
-    var type: HackathonType
-    var eligibility: String
-    var rules: String
-    var teamSize: Int
-    var prizes: String? // Optional prizes
-    
-    struct HackathonDates: Codable {
-        var start: Date
-        var end: Date
-    }
-    
-    enum HackathonType: String, Codable {
-        case online = "Online"
-        case offline = "Offline"
-    }
+    var id: String = UUID().uuidString
+    var bannerImage: String? // URL or image name
+    var name: String
+    var tagline: String
+    var description: String
+    var dates: HackathonDates
+    var type: HackathonType
+    var eligibility: String
+    var rules: [String]
+    var teamSize: Int
+    var prizes: [String]?
+    
+    struct HackathonDates: Codable {
+        var start: Date
+        var end: Date
+    }
+    
+    enum HackathonType: String, Codable {
+        case online = "Online"
+        case offline = "Offline"
+        case hybrid = "Hybrid"
+    }
 }
-
